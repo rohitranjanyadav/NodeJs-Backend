9fe14a8afc3cad3e2135cfe66d7eafabeeed1e35
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-06-05 23:18:25 +0545

    feat: refactor book management by implementing routing and middleware

--- Express/Express-Book-Store/index.js
+++ Express/Express-Book-Store/index.js
@@ -1,24 +1,14 @@
 const express = require("express");
-const fs = require("node:fs");
+const bookRouter = require("./routes/book.routes");
+const { loggerMiddleware } = require("./middlewares/logger");
 
 const app = express();
 const PORT = 4000;
 
-// In Memory DB
-const books = [
-  { id: 1, title: "Book One", author: "Author One" },
-  { id: 2, title: "Book Two", author: "Author Two" },
-];
-
 // Middleware
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  const log = `\n[${Date.now()}] ${req.method} ${req.path}`;
-  fs.appendFileSync("logs.txt", log, "utf-8");
-  console.log("Logged!");
-  next();
-});
+app.use(loggerMiddleware);
 
 function customMiddleware(req, res, next) {
   console.log("I am a custom Middleware");
@@ -41,57 +31,7 @@ app.use("/books", function (req, res, next) {
 // })
 
 // Routes
-app.get("/books", customMiddleware, (req, res) => {
-  res.json(books);
-});
-
-app.get("/books/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-
-  // Bad Request
-  if (isNaN(id))
-    return res.status(400).json({ error: `id must be of type number` });
-
-  const book = books.find((b) => b.id === id);
-
-  if (!book)
-    return res.status(404).json({ error: `Book with id ${id} does not exist` });
-
-  return res.json(book);
-});
-
-app.post("/books", (req, res) => {
-  const { title, author } = req.body;
-
-  if (!title || title === "")
-    return res.status(400).json({ error: "title is requires" });
-  if (!author || author === "")
-    return res.status(400).json({ error: "author is requires" });
-
-  const id = books.length + 1;
-
-  const book = { id, title, author };
-
-  books.push(book);
-
-  return res.status(201).json({ message: "Book created successfully", book });
-});
-
-app.delete("/books/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-
-  if (isNaN(id))
-    return res.status(400).json({ error: `id must be of type number` });
-
-  const indexToDelete = books.findIndex((e) => e.id === id);
-
-  if (indexToDelete < 0)
-    return res.status(404).json({ error: `book with id ${id} does not exist` });
-
-  const deletedBook = books.splice(indexToDelete, 1);
-
-  return res.status(200).json({ message: "Book deleted", deletedBook });
-});
+app.use("/books", bookRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
