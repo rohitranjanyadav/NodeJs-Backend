de9ff57976a6e7e1776a6fafb2d820fbebe8eea9
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-06-07 19:06:24 +0545

    chore: initialize package.json for Node-ORM-1 with dependencies

---
+++ Node-ORM-1/index.js
@@ -0,0 +1,21 @@
+require("dotenv/config");
+const db = require("./db");
+const { usersTable } = require("./drizzle/schema");
+
+
+
+async function getAllUsers() {
+  const users = await db.select().from(usersTable);
+  console.log(`Users in DB`, users);
+  return users;
+}
+
+async function createUser({ id, name, email }) {
+  await db.insert(usersTable).values({
+    id,
+    name,
+    email,
+  });
+}
+
+getAllUsers();
